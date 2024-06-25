e657491ea0be8037b4f3784578ba5370fa80851e
Ahmik Virani <ahmikvirani44@gmail.com>
2024-06-25 18:26:54 +0530

    Added search bar in the problems page

--- online-judge/frontend/src/problems.jsx
+++ online-judge/frontend/src/problems.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 function Problems() {
     const [problems, setProblems] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const fetchProblems = async () => {
@@ -28,29 +29,41 @@ function Problems() {
     };
 
     return (
-        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-            <div className="w-50 bg-white rounded p-3">
-                <Link to='/create_problem' className='btn btn-success mb-2'>Add +</Link>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Problem Title</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {problems.map((problem) => (
-                            <tr key={problem._id}>
-                                <Link to={`/view_problem/${problem._id}`}>{problem.problem_title}</Link>
-                                {/* //<td>{problem.problem_title}</td> */}
-                                <td>
-                                    <Link to={`/update_problem/${problem._id}`} className='btn btn-success'>Update</Link>
-                                    <button className='btn btn-danger' onClick={(e) => handleDelete(problem._id)}>Delete</button>
-                                </td>
+        <div>
+            <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+                <div className="w-50 bg-white rounded p-3">
+                    <Link to='/create_problem' className='btn btn-success mb-2'>Add +</Link>
+                    <input type="text" placeholder="Search here..." className="mb-3 form-control" onChange={(e)=>{setSearchTerm(e.target.value)}} />
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Problem Title</th>
+                                <th>Action</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {problems
+                            .filter((problem) => {
+                                if(searchTerm == ""){
+                                    return problem;
+                                }else if(problem.problem_title.toLowerCase().includes(searchTerm.toLocaleLowerCase())){
+                                    return problem;
+                                }
+                            })
+                            .map((problem) => (
+                                <tr key={problem._id}>
+                                    <td>
+                                        <Link to={`/view_problem/${problem._id}`}>{problem.problem_title}</Link>
+                                    </td>
+                                    <td>
+                                        <Link to={`/update_problem/${problem._id}`} className='btn btn-success'>Update</Link>
+                                        <button className='btn btn-danger' onClick={(e) => handleDelete(problem._id)}>Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
